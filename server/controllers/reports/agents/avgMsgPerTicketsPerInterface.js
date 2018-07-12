8b255ba5f6a052db5f2dbcc215941b63de7ceab9
Franklin Esquivel <franklin.esquivel@outlook.com>
2018-07-12 00:27:55 -0600

    Ups miss this once

--- server/controllers/reports/agents/avgMsgPerTicketsPerInterface.js
+++ server/controllers/reports/agents/avgMsgPerTicketsPerInterface.js
@@ -2,6 +2,7 @@ const ObjectId = require('mongodb').ObjectId;
 const dbContext = require('../../../src/connection');
 
 const moment = require('moment');
+const utils = require('../utils');
 
 moment().locale('es_sv')
 
@@ -11,15 +12,23 @@ module.exports = (req, res) => {
         dbContext(async (db, client) => {
     
             let frmData = {
-                initDate: moment(`${req.body.intervalData.init.date} ${req.body.intervalData.init.time}`, 'YYYY-MM-DD H:mm'),
-                endDate: moment(`${req.body.intervalData.end.date} ${req.body.intervalData.end.time}`, 'YYYY-MM-DD H:mm'),
+                init: {
+                    date: req.body.intervalData.init.date,
+                    time: req.body.intervalData.init.time,
+                    moment: moment(`${req.body.intervalData.init.date} ${req.body.intervalData.init.time}`, 'YYYY-MM-DD HH:mm')
+                },
+                end: {
+                    date: req.body.intervalData.end.date,
+                    time: req.body.intervalData.end.time,
+                    moment: moment(`${req.body.intervalData.end.date} ${req.body.intervalData.end.time}`, 'YYYY-MM-DD HH:mm')
+                },
                 interval: {
                     type: req.body.interval.split(':')[0].toLowerCase(),
                     cant: parseInt(req.body.interval.split(':')[1])
                 }
             }
             
-            let data = {}, $flag = true, auxDate = [], msgData = [];
+            let data = {}, $flag = true, auxDate = [frmData.init.moment, null], msgData = [];
         
             if(ObjectId.isValid(req.body.account_id)){
                 const account = await db.collection("accounts").findOne({_id: ObjectId(req.body.account_id)});
@@ -29,30 +38,8 @@ module.exports = (req, res) => {
                     const $tickets = await db.collection("tickets").find({ account: ObjectId(account._id) }).toArray();
                     
                     while($flag){
-                        
-                        if(auxDate.length === 0){
-                            let auxLimit = frmData.initDate.clone();
-                            auxLimit.add(frmData.interval.cant, frmData.interval.type)
-                            
-                            auxDate = [frmData.initDate.clone(), auxLimit.clone()];
-                        }else{
-                            let _aux = auxDate[1].clone();
-                            
-                            auxDate[0] = auxDate[1].clone();
-                            auxDate[0].add(1, 'm');
-                            // auxDate[0].add(1, frmData.interval.type);
-                            // _aux = auxDate[0].clone();
-                            
-                            _aux.add(frmData.interval.cant, frmData.interval.type);
-                            
-                            if(_aux >= frmData.endDate){
-                                auxDate[1] = frmData.endDate.clone();
-                                $flag = false;
-                            }else{
-                                // auxDate[0].add(1, frmData.interval.type);
-                                auxDate[1] = _aux.clone();
-                            }
-                        }
+                        auxDate = utils.getInterval(auxDate, frmData.init, frmData.end, frmData.interval)
+                        $flag = (auxDate[1].format('YYY-MM-DD HH:mm') !== frmData.end.moment.format('YYY-MM-DD HH:mm'))
                         
                         const dateKey = `${moment(auxDate[0]).format('YYYY-MM-DD_HH:mm')}_-_${moment(auxDate[1]).format('YYYY-MM-DD_HH:mm')}`;
                         msgData[dateKey] = [];
